4dba9aa541e9767b30f4d48d0730b29733817d18
DrFlower <303333530@qq.com>
2022-11-02 02:11:31 +0800

    202 homework 2 build

--- Homework_202/Assignment2/prt/build/ext_build/nanogui/nanogui_resources.h
+++ Homework_202/Assignment2/prt/build/ext_build/nanogui/nanogui_resources.h
@@ -11,3 +11,19 @@ extern const uint8_t inconsolata_regular_ttf[];
 
 extern uint32_t inconsolata_regular_ttf_size;
 
+extern const uint8_t roboto_bold_ttf[];
+
+extern uint32_t roboto_bold_ttf_size;
+
+extern const uint8_t roboto_regular_ttf[];
+
+extern uint32_t roboto_regular_ttf_size;
+
+extern const uint8_t imageview_fragment_gl[];
+
+extern uint32_t imageview_fragment_gl_size;
+
+extern const uint8_t imageview_vertex_gl[];
+
+extern uint32_t imageview_vertex_gl_size;
+
