1ac8dc57f423d38c5a2e91b34ff2d8e9cd94d9e9
DrFlower <303333530@qq.com>
2022-11-11 00:42:14 +0800

    202 homework 2

--- Homework_202/Assignment2/homework2/src/shaders/prtShader/prtFragment.glsl
+++ Homework_202/Assignment2/homework2/src/shaders/prtShader/prtFragment.glsl
@@ -4,6 +4,20 @@ precision mediump float;
 
 varying highp vec3 vColor;
 
+vec3 toneMapping(vec3 color){
+    vec3 result;
+
+    for (int i=0; i<3; ++i) {
+        if (color[i] <= 0.0031308)
+            result[i] = 12.92 * color[i];
+        else
+            result[i] = (1.0 + 0.055) * pow(color[i], 1.0/2.4) - 0.055;
+    }
+
+    return result;
+}
+
 void main(){
-  gl_FragColor = vec4(vColor, 1.0);
+  vec3 color = toneMapping(vColor); 
+  gl_FragColor = vec4(color, 1.0);
 }
